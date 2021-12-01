756b4418cd249cd2f31e9adf07464f6cc7084e78
Dominic Black <dpblack96@gmail.com>
2021-11-30 23:57:45 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) 
+console.log(random)
+
+const num3 = 4
+const num4 = 1
+let mod = num3 / num4
+
+const max = Math.max(5,10,15,20)
